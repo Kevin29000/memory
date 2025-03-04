ab0d6dc3efbce563c532bc42e26cd028b0460ec2
Kevin CORRE <kevin.corre2025@campus-eni.fr>
2025-03-04 13:30:21 +0100

    third commmit

--- script.js
+++ script.js
@@ -1,41 +1,89 @@
-$('#registrationForm').on('submit', function(event) {
-    event.preventDefault();
+$(document).ready(function() {
 
-    let username = $('#usernameRegistration').val();
-    let email = $('#emailRegistration').val();
-    let password = $('#passwordRegistration').val();
-
-    let user = {
-        username: username,
-        email: email,
-        password: password
-    };
-
-    // Vérifie si l'email est déjà présente dans le localStorage
-    if (findUserByEmail(email)) {
-        alert('L\'email : ' + email + ' est déjà utilisé !' );
-        return;
-    }
-
-    // userId est une donnée indépendante dans le localStorage est sert uniquement à définir l'id de l'utilisateur
-    let userId = localStorage.getItem('userId');
+    // Inscription
+    $('#registrationForm').on('submit', function(event) {
+        event.preventDefault();
     
-    // Définit le userID dans le localStorage
-    if (!userId) {
-        userId = 1;
-    } else {
-        userId ++;
-    }
+        let username = $('#usernameRegistration').val();
+        let email = $('#emailRegistration').val();
+        let password = $('#passwordRegistration').val();
+    
+        let user = {
+            username: username,
+            email: email,
+            password: password
+        };
+    
+        // Vérifie si l'email est déjà présente dans le localStorage
+        if (findUserByEmail(email)) {
+            alert(`L'email : ${email} est déjà utilisé !` );
+            return;
+        }
+    
+        // userId est une donnée indépendante dans le localStorage est sert uniquement à définir l'id de l'utilisateur
+        let userId = localStorage.getItem('userId');
+        
+        // Définit le userID dans le localStorage
+        if (!userId) {
+            userId = 1;
+        } else {
+            userId ++;
+        }
+    
+        localStorage.setItem(`user${userId}`, JSON.stringify(user));
+        console.log(`Utilisateur ajouté : user${userId}`, JSON.stringify(user));
+        // Met à jour la valeur de userId pour la prochaine inscription
+        localStorage.setItem('userId', userId);
+    });
 
-    localStorage.setItem(`user${userId}`, JSON.stringify(user));
-    console.log(`Utilisateur ajouté : user${userId}`, JSON.stringify(user));
-    // Met à jour la valeur de userId pour la prochaine inscription
-    localStorage.setItem('userId', userId);
+    // Vérification des inputs
+    $('#usernameRegistration').on('input', function() {
+        validateUsername($(this).val());
+    });
+
+    $('#emailRegistration').on('input', function() {
+        validateEmail($(this).val());
+    });
+
+    $('#passwordRegistration').on('input', function() {
+        validatePassword($(this).val());
+    });
+
+    // Login
+    $('#loginForm').on('submit', function(event) {
+        event.preventDefault();
+    
+        let email = $('#loginEmail').val();
+        let password = $('#loginPassword').val();
+    
+        // console.log(email, password);
+    
+        if(login(email, password)) {
+            console.log('User trouvé');
+        } else {
+            $('#loginError').text('L\'email ou le mot de passe est incorrect !')
+        }
+    });
+
 });
+
+function login(email, password) {
+    for (let i = 0; i < localStorage.length; i++) {
+        // Récupère la clé à l'index i dans le localSotrage
+        let key = localStorage.key(i);
+
+        // Prend le user lié à la clé
+        let user = JSON.parse(localStorage.getItem(key));
+
+        if (user.email === email && user.password === password) {
+            localStorage.setItem('currentUser', JSON.stringify(user));
+            break;
+        }
+    }
+}
 
 function findUserByEmail(email) {
     let foundUser = null;
-
     for (let i = 0; i < localStorage.length; i++) {
         // Récupère la clé à l'index i dans le localSotrage
         let key = localStorage.key(i);
@@ -48,62 +96,37 @@ function findUserByEmail(email) {
             break;
         }
     }
-
     return foundUser;
 }
 
-
 // Vérifie l'input username
-$('#usernameRegistration').on('input', function() {
-    if ($(this).val().length >= 3) {
-        $('#usernameRegistrationImg').attr('src', 'assets/images/check.svg')
-    } else {
-        $('#usernameRegistrationImg').attr('src', 'assets/images/error.svg')
-    }
-});
+function validateUsername(username) {
+    let isValid = username.length >= 3;
+    updateValidationIcon('#usernameRegistrationImg', isValid);
+}
 
 // Vérifie l'input email
-$('#emailRegistration').on('input', function() {
-    const email = $(this).val();
-
-    if (/[a-zA-Z0-9._%+-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,}/.test(email)) {
-        $('#emailRegistrationImg').attr('src', 'assets/images/check.svg')
-    } else {
-        $('#emailRegistrationImg').attr('src', 'assets/images/error.svg')
-    }
-    
-});
+function validateEmail(email) {
+    let regex = /[a-zA-Z0-9._%+-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,}/;
+    let isValid = regex.test(email);
+    updateValidationIcon('#emailRegistrationImg', isValid);
+}
 
 // Vérifie l'input password
-$('#passwordRegistration').on('input', function() {
-    const password = $('#passwordRegistration').val();
-    // console.log('Le mdp est : '+ password);
-    let letters = password.split("");
+function validatePassword(password) {
+    // /\d/ est la regex pour chercher si la chaine contient un nombre, revient au même que /[0-9]/
+    let hasNumber = /\d/.test(password);
+    // le ^ à cet endroit veut dire tout sauf min, maj, chiffre, donc un caractère spécial
+    let hasSpecialChar = /[^a-zA-Z0-9]/.test(password);
+    let isValidLength = password.length >= 6;
 
-    let hasNumber = false;
-    let length = false;
-    let hasSpecialChar = false;
+    let isValid = hasNumber && isValidLength && hasSpecialChar;
 
-    for (let i = 0; i < letters.length; i++) {
-        if (!isNaN(letters[i])) {
-            hasNumber = true;
-            // le ^ à cet endroit veut dire tout sauf min, maj, chiffre, donc un caractère spécial
-        } else if (/[^a-zA-Z0-9]/.test(letters[i])) {
-            hasSpecialChar = true;
-        } 
-    }
+    updateValidationIcon('#passwordRegistrationImg', isValid);
+}
 
-    if (letters.length >= 6) {length = true}
-
-    if (hasNumber && length && hasSpecialChar) {
-        $('#passwordRegistrationImg').attr('src', 'assets/images/check.svg')
-    } else {
-        $('#passwordRegistrationImg').attr('src', 'assets/images/error.svg')
-    }
-});
-
-
-// let test = JSON.parse(localStorage.getItem('user1'));
-// console.log(test.username, test.email, test.password);
-
-// localStorage.clear();
+// Modifie l'image de validation du formulaire
+function updateValidationIcon(imgId, isValid) {
+    let icon = isValid ? 'assets/images/check.svg' : 'assets/images/error.svg';
+    $(imgId).attr('src', icon);
+}
